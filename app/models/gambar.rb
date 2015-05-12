ded5bf09f1228379caacb598d3792e3a59ab7abf
Cindy <cindyraissa_88@me.com>
2015-05-12 10:39:55 +0700

    update ukuran gambar

--- app/models/gambar.rb
+++ app/models/gambar.rb
@@ -3,4 +3,5 @@ class Gambar < ActiveRecord::Base
 
   has_attached_file :nama_gambar, :styles => {:medium => "400x200>", :thumb => "200x100>", :default_url => ""}
   validates_attachment_content_type :nama_gambar, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_size :nama_gambar, :less_than => 200.kilobytes
 end
